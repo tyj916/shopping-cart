a8e8e18754173b9c986efb4f0411ef172fde2d5d
Tan Yi Jun <tanyijun6@gmail.com>
2024-08-27 21:56:22 +0800

    Add product data

---
+++ src/components/Product.jsx
@@ -0,0 +1,9 @@
+function Product({title, price, description, category, imageURL}) {
+  return (
+    <li>
+      <p>{title}</p>
+    </li>
+  )
+}
+
+export default Product;
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,53 @@
+import { useEffect, useState } from "react";
+import Product from "./Product";
+
 function Shop() {
+  const [products, setProducts] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProductsData = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        if (!response.ok) {
+          throw new Error(`HTTP error: Status ${response.status}`);
+        }
+        const productsData = await response.json();
+        setProducts(productsData);
+      } catch(err) {
+        setError(err.message);
+        setProducts(null);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchProductsData();
+  }, []);
+
   return (
     <div>
       <h1>Shop</h1>
+      <ul className="products">
+        {
+          loading ? <p>Loading...</p> :
+          error ? <p>A network error was encountere</p> :
+          products && products.length === 0 ? <p>No product.</p> :
+          products && products.map(product => {
+            return (
+              <Product 
+                key={product.id}
+                title={product.title}
+                price={product.price}
+                category={product.category}
+                description={product.description}
+                imageURL={product.image}
+              />
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
