6f788ed0bead027319b1b738a87c4d6c0be133db
Tan Yi Jun <tanyijun6@gmail.com>
2024-08-28 21:24:57 +0800

    Add plus and minus count function

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
+
 function AddToCart() {
+  const [count, setCount] = useState(1);
+
+  function handleCountChange(e) {
+    setCount(+e.target.value);
+  }
+
+  function addCount() {
+    setCount(count + 1);
+  }
+
+  function minusCount() {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  }
+
   return (
     <div className='add-to-cart-controller'>
       <span className='quantity-controller'>
-        <button type='button'>-</button>
-        <input type="tel" />
-        <button type='button'>+</button>
+        <button type='button' onClick={minusCount}>-</button>
+        <input type="tel" aria-label='count' value={count} onChange={handleCountChange} />
+        <button type='button' onClick={addCount}>+</button>
       </span>
       <button type='button'>Add to cart</button>
     </div>
---
+++ src/tests/AddToCart.test.jsx
@@ -0,0 +1,48 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import AddToCart from "../components/AddToCart";
+import userEvent from "@testing-library/user-event";
+
+describe('Add to cart component', () => {
+  it('renders add to cart button with number control', () => {
+    const { container } = render(<AddToCart />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('renders value 2 after add button is clicked', async () => {
+    const user = userEvent.setup();
+
+    render(<AddToCart />);
+    const button = screen.getByRole('button', { name: '+' });
+
+    await user.click(button);
+
+    expect(+screen.getByLabelText('count').value).toBe(2);
+  });
+
+  it('should minus 1 count after minus button is clicked', async () => {
+    const user = userEvent.setup();
+
+    render(<AddToCart />);
+    const addButton = screen.getByRole('button', { name: '+' });
+    const minusButton = screen.getByRole('button', { name: '-' });
+    const input = screen.getByLabelText('count');
+
+    await user.click(addButton);
+    await user.click(minusButton);
+
+    expect(+input.value).toBe(1);
+  });
+
+  it('should not lower than 1', async () => {
+    const user = userEvent.setup();
+
+    render(<AddToCart />);
+    const minusButton = screen.getByRole('button', { name: '-' });
+    const input = screen.getByLabelText('count');
+
+    await user.click(minusButton);
+
+    expect(+input.value).toBe(1);
+  });
+});
